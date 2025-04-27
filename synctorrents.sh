11e5f54db465e49b4fac8234b0c9d65dcbaf243d
Shane <strider2112@users.noreply.github.com>
2025-04-27 00:25:10 -0600

    Update synctorrents.sh
    Update with ssh-key options for security

--- synctorrents.sh
+++ synctorrents.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
+
 # Login details for remote server
 login="your_user"
 pass="your_password"
 host="your_server_address"
 
+# Use ssh_key for passwordless access
+# Set use_key to "true"
+use_key=false
+ssh_key="/home/user/.ssh/key"
+eval "$(ssh-agent -s)"
+ssh-add "$ssh_key"
+
+# Set args if key is used
+if [ "$use_key" = true ]; then
+	ssh_args="ssh -a -x -i $ssh_key"
+else
+	ssh_args=""
+fi
+
 # Remote download finished location, use symlinks on remote server
-remote_finished="/remote/path/to/finished"
+remote_finished="/remote/path/to/finished/"
 
 # Local location for downloads
-local_downloads="/local/path/for/downloads"
+local_downloads="/local/path/for/downloads/"
 
 # If ltfp is not running, start the transfer
 if [ -e synctorrent.lock ]
@@ -18,11 +33,18 @@ then
 else
 	# Create file to track if lftp is running
 	touch synctorrent.lock
-	lftp -p 22 -u $login,$pass sftp://$host << EOF
+	lftp -p 22 -u $login,$pass sftp://$host "$ssh_args" << EOF
 	set mirror:use-pget-n 3
 	mirror -c -P5 --log=movies-sync.log --Remove-source-files $remote_finished $local_downloads
 	quit
 EOF
+	# Set permissions for Sonarr / Radarr
+	chown -R nobody:users "$local_downloads"
+	chmod -R 777 "$local_downloads"
+	
+	# Kill the ssh-agent
+	pkill ssh-agent
+ 
 	rm -f synctorrent.lock
 	exit 0
 fi
